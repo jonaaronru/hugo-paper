645273c2a8cd1c2d63a95d43f7f7276d524e6691
imperatrona <xb2w1z@gmail.com>
2022-03-17 16:55:53 +0300

    Moved color scheme logic to separate file

---
+++ static/script.js
@@ -0,0 +1,14 @@
+let sysDark = window.matchMedia('(prefers-color-scheme: dark)');
+let darkVal = localStorage.getItem('dark');
+
+function setDark(isDark) {
+  document.body.classList[isDark ? 'add' : 'remove']('dark');
+  localStorage.setItem('dark', isDark ? 'yes' : 'no');
+  if (document.body.classList.contains('not-ready')) {
+    requestAnimationFrame(() => document.body.classList.remove('not-ready'));
+  }
+};
+
+setDark(darkVal ? darkVal === 'yes' : sysDark.matches);
+
+sysDark.addEventListener('change', (event) => setDark(event.matches));
